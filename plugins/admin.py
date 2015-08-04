26fade9678130672704bcbdf4617a62d55f8e1b4
Filipe Pina <fopina@skmobi.com>
2015-08-04 16:40:27 +0100

    admin plugin using prefix command for /msg updated to 0.1.14

--- plugins/admin.py
+++ plugins/admin.py
@@ -1,5 +1,5 @@
 import tgbot
-from twx.botapi import Error
+from twx.botapi import Error, ForceReply
 import hashlib
 
 
@@ -10,18 +10,17 @@ class AdminPlugin(tgbot.TGPluginBase):
 
     def list_commands(self):
         return (
-            tgbot.TGCommandBase('users', self.list_users, 'List known users'),
-            tgbot.TGCommandBase('chats', self.list_chats, 'List active chats'),
-            tgbot.TGCommandBase('more', self.more, 'List 10 more hits'),
-            tgbot.TGCommandBase('msg', self.msg, 'Send message to user/chat'),
-            tgbot.TGCommandBase('newpass', self.newpass, 'Change admin password'),
-            tgbot.TGCommandBase('auth', self.auth, 'Authenticate as admin'),
+            tgbot.TGCommandBase('users', self.list_users, 'List known users', printable=False),
+            tgbot.TGCommandBase('chats', self.list_chats, 'List active chats', printable=False),
+            tgbot.TGCommandBase('more', self.more, 'List 10 more hits', printable=False),
+            tgbot.TGCommandBase('msg', self.msg, 'Send message to user/chat', prefix=True, printable=False),
+            tgbot.TGCommandBase('newpass', self.newpass, 'Change admin password', printable=False),
+            tgbot.TGCommandBase('auth', self.auth, 'Authenticate as admin', printable=False),
         )
 
     def __is_admin(self, message):
         obj = self.read_data(message.chat.id, key2='ADMIN')
         return obj is True
-
 
     def auth(self, bot, message, text):
         if self.__is_admin(message):
@@ -33,68 +32,95 @@ class AdminPlugin(tgbot.TGPluginBase):
 
             if hashlib.sha256(text).hexdigest() == phash:
                 self.save_data(message.chat.id, key2='ADMIN', obj=True)
-                bot.tg.send_message(message.chat.id, 'Welcome :-)')
+                bot.tg.send_message(message.chat.id, u'Welcome \U0001F60F')
 
     def newpass(self, bot, message, text):
         if self.__is_admin(message) and text:
             self.save_data('ADMINPWD', obj=hashlib.sha256(text).hexdigest())
             bot.tg.send_message(message.chat.id, 'Password updated to:\n' + text)
 
+    def list(self, bot, message, model_cls, page=1):
+        msg = ''
+        cnt = 0
+        for u in model_cls.select().paginate(page, 10):
+            cnt += 1
+
+            nid = u.id
+            if nid < 0:
+                nid = 'N' + str(nid * -1)  # much faster than abs..
+
+            if model_cls == tgbot.models.User:
+                msg += '/msg%s - %s %s\n' % (
+                    nid,
+                    u.first_name,
+                    u.last_name,
+                )
+            else:
+                msg += '/msg%s - %s\n' % (
+                    nid,
+                    u.title,
+                )
+
+        if cnt == 10:
+            msg += 'There are more, type /more to list 10 more results'
+            self.save_data(message.chat.id, obj={'more': model_cls.__name__, 'page': page})
+        elif cnt == 0:
+            msg = 'Zero...'
+            self.save_data(message.chat.id)
+        else:
+            self.save_data(message.chat.id)
+
+        bot.tg.send_message(message.chat.id, msg)
+
     def list_users(self, bot, message, text):
         if not self.__is_admin(message):
             return
+        self.list(bot, message, tgbot.models.User)
 
-        msg = ''
-        cnt = 0
-        for u in tgbot.models.User.select().paginate(1, 10):
-            cnt += 1
-            msg += '%s - %s %s\n' % (
-                u.id,
-                u.first_name,
-                u.last_name,
-            )
-
-        if cnt == 10:
-            msg += 'There are more users, type /more to list 10 more results'
-            self.save_data(message.chat.id, obj={'more': 'user', 'page': 1})
-        elif cnt == 0:
-            msg = 'No users...'
-
-        bot.tg.send_message(message.chat.id, msg)
-
-    def list_chats(self, bot, message, text):
+    def list_chats(self, bot, message, text, page=1):
         if not self.__is_admin(message):
             return
-
-        msg = ''
-        cnt = 0
-        for u in tgbot.models.GroupChat.select().paginate(1, 10):
-            cnt += 1
-            msg += '%s - %s\n' % (
-                u.id,
-                u.title,
-            )
-
-        if cnt == 10:
-            msg += 'There are more groups, type /more to list 10 more results'
-            self.save_data(message.chat.id, obj={'more': 'chat', 'page': 1})
-        elif cnt == 0:
-            msg = 'No chats...'
-
-        bot.tg.send_message(message.chat.id, msg)
+        self.list(bot, message, tgbot.models.GroupChat)
 
     def msg(self, bot, message, text):
         if not self.__is_admin(message):
             return
 
+        if text == '':
+            bot.tg.send_message(message.chat.id, 'Use /users or /chats to list available ids')
+            return
+
         p = text.find(' ')
+        if p < 0:
+            self.save_data(message.chat.id, key2='msg', obj=text)
+            m = bot.tg.send_message(
+                message.chat.id,
+                'And say what?',
+                reply_to_message_id=message.message_id,
+                reply_markup=ForceReply.create(selective=True)
+            ).wait()
+            self.need_reply(self.forward_message, message, out_message=m, selective=True)
+            return
+
         cid = text[:p]
         msg = text[p + 1:]
-        m = bot.tg.send_message(cid, msg).wait()
+
+        self.forward_message(bot, message, msg, dst=cid)
+
+    def forward_message(self, bot, message, msg, dst=None):
+        if dst is None:
+            dst = self.read_data(message.chat.id, key2='msg')
+            self.save_data(message.chat.id, key2='msg')
+
+        if dst is None:
+            bot.tg.send_message(message.chat.id, 'Something went wrong...')
+            return
+
+        m = bot.tg.send_message(dst, msg).wait()
         if isinstance(m, Error):
             bot.tg.send_message(message.chat.id, "Failed to send message:\n%s (%d)" % (m.description, m.error_code))
         else:
-            bot.tg.send_message(message.chat.id, "'%s' sent to %s" % (msg, cid))
+            bot.tg.send_message(message.chat.id, "'%s' sent to %s" % (msg, dst))
 
     def more(self, bot, message, text):
         if not self.__is_admin(message):
@@ -106,32 +132,8 @@ class AdminPlugin(tgbot.TGPluginBase):
             bot.tg.send_message(message.chat.id, 'No pending query...')
             return
 
-        msg = ''
-        cnt = 0
-
         more['page'] += 1
-        if more['more'] == 'chat':
-            for u in tgbot.models.GroupChat.select().paginate(more['page'], 10):
-                cnt += 1
-                msg += '%s - %s\n' % (
-                    u.id,
-                    u.title,
-                )
-        elif more['more'] == 'user':
-            for u in tgbot.models.User.select().paginate(more['page'], 10):
-                cnt += 1
-                msg += '%s - %s %s\n' % (
-                    u.id,
-                    u.first_name,
-                    u.last_name,
-                )
-
-        if cnt == 10:
-            msg += 'Type /more to list 10 more results'
-            self.save_data(message.chat.id, obj=more)
-        else:
-            if cnt == 0:
-                msg = 'Nothing left...'
-            self.save_data(message.chat.id)
-
-        bot.tg.send_message(message.chat.id, msg)
+        if more['more'] == 'GroupChat':
+            self.list(bot, message, tgbot.models.GroupChat, page=more['page'])
+        elif more['more'] == 'User':
+            self.list(bot, message, tgbot.models.User, page=more['page'])
--- tests/test_admin.py
+++ tests/test_admin.py
@@ -66,12 +66,12 @@ class AdminPluginTest(plugintest.PluginTestCase):
         self.assertRaises(AssertionError, self.last_reply, self.bot)
 
         self.receive_message('/auth changeme')
-        self.assertReplied(self.bot, 'Welcome :-)')
+        self.assertReplied(self.bot, u'Welcome \U0001F60F')
 
         self.receive_message('/users')
         self.assertReplied(
             self.bot,
-            '1 - John Doe\n'
+            '/msg1 - John Doe\n'
         )
 
         self.receive_message('hey', sender={
@@ -84,8 +84,8 @@ class AdminPluginTest(plugintest.PluginTestCase):
         self.assertReplied(
             self.bot,
             '''\
-1 - John Doe
-2 - Jane Doe
+/msg1 - John Doe
+/msg2 - Jane Doe
 ''')
 
     def test_groups(self):
@@ -93,13 +93,10 @@ class AdminPluginTest(plugintest.PluginTestCase):
         self.assertRaises(AssertionError, self.last_reply, self.bot)
 
         self.receive_message('/auth changeme')
-        self.assertReplied(self.bot, 'Welcome :-)')
+        self.assertReplied(self.bot, u'Welcome \U0001F60F')
 
         self.receive_message('/chats')
-        self.assertReplied(
-            self.bot,
-            'No chats...'
-        )
+        self.assertReplied(self.bot, 'Zero...')
 
         self.receive_message(
             'hey',
@@ -110,11 +107,7 @@ class AdminPluginTest(plugintest.PluginTestCase):
         )
 
         self.receive_message('/chats')
-        self.assertReplied(
-            self.bot,
-            '''\
-1 - chat 1
-''')
+        self.assertReplied(self.bot, '/msg1 - chat 1\n')
 
         self.receive_message(
             'hey',
@@ -128,8 +121,8 @@ class AdminPluginTest(plugintest.PluginTestCase):
         self.assertReplied(
             self.bot,
             '''\
-1 - chat 1
-2 - chat 2
+/msg1 - chat 1
+/msg2 - chat 2
 ''')
 
         self.leave_chat(
@@ -140,60 +133,51 @@ class AdminPluginTest(plugintest.PluginTestCase):
         )
 
         self.receive_message('/chats')
-        self.assertReplied(
-            self.bot,
-            '''\
-2 - chat 2
-''')
+        self.assertReplied(self.bot, '/msg2 - chat 2\n')
 
     def test_more_users(self):
         self.receive_message('/more')
         self.assertRaises(AssertionError, self.last_reply, self.bot)
 
         self.receive_message('/auth changeme')
-        self.assertReplied(self.bot, 'Welcome :-)')
+        self.assertReplied(self.bot, u'Welcome \U0001F60F')
 
         self.receive_message('/more')
         self.assertReplied(self.bot, 'No pending query...')
 
         self.receive_message('/users')
-        self.assertReplied(
-            self.bot,
-            '1 - John Doe\n'
-        )
+        self.assertReplied(self.bot, '/msg1 - John Doe\n')
 
-        for i in xrange(2, 15):
+        for i in xrange(2, 11):
             self.receive_message('hey', sender={
                 'id': i,
                 'first_name': 'Jane',
                 'last_name': str(i),
             })
+        self.receive_message('hey', sender={
+            'id': -1,
+            'first_name': 'Jane',
+            'last_name': '-1',
+        })
 
         self.receive_message('/users')
         self.assertReplied(
             self.bot,
             '''\
-1 - John Doe
-2 - Jane 2
-3 - Jane 3
-4 - Jane 4
-5 - Jane 5
-6 - Jane 6
-7 - Jane 7
-8 - Jane 8
-9 - Jane 9
-10 - Jane 10
-There are more users, type /more to list 10 more results''')
+/msgN1 - Jane -1
+/msg1 - John Doe
+/msg2 - Jane 2
+/msg3 - Jane 3
+/msg4 - Jane 4
+/msg5 - Jane 5
+/msg6 - Jane 6
+/msg7 - Jane 7
+/msg8 - Jane 8
+/msg9 - Jane 9
+There are more, type /more to list 10 more results''')
 
         self.receive_message('/more')
-        self.assertReplied(
-            self.bot,
-            '''\
-11 - Jane 11
-12 - Jane 12
-13 - Jane 13
-14 - Jane 14
-''')
+        self.assertReplied(self.bot, '/msg10 - Jane 10\n')
         self.receive_message('/more')
         self.assertReplied(self.bot, 'No pending query...')
 
@@ -202,15 +186,15 @@ There are more users, type /more to list 10 more results''')
         self.assertRaises(AssertionError, self.last_reply, self.bot)
 
         self.receive_message('/auth changeme')
-        self.assertReplied(self.bot, 'Welcome :-)')
+        self.assertReplied(self.bot, u'Welcome \U0001F60F')
 
         self.receive_message('/more')
         self.assertReplied(self.bot, 'No pending query...')
 
         self.receive_message('/chats')
-        self.assertReplied(self.bot, 'No chats...')
+        self.assertReplied(self.bot, 'Zero...')
 
-        for i in xrange(1, 22):
+        for i in xrange(-1, 20):
             self.receive_message('hey', chat={
                 'id': i,
                 'title': 'chat %d' % i,
@@ -220,25 +204,21 @@ There are more users, type /more to list 10 more results''')
         self.assertReplied(
             self.bot,
             '''\
-1 - chat 1
-2 - chat 2
-3 - chat 3
-4 - chat 4
-5 - chat 5
-6 - chat 6
-7 - chat 7
-8 - chat 8
-9 - chat 9
-10 - chat 10
-There are more groups, type /more to list 10 more results''')
+/msgN1 - chat -1
+/msg0 - chat 0
+/msg1 - chat 1
+/msg2 - chat 2
+/msg3 - chat 3
+/msg4 - chat 4
+/msg5 - chat 5
+/msg6 - chat 6
+/msg7 - chat 7
+/msg8 - chat 8
+There are more, type /more to list 10 more results''')
 
         self.receive_message('/more')
         self.receive_message('/more')
-        self.assertReplied(
-            self.bot,
-            '''\
-21 - chat 21
-''')
+        self.assertReplied(self.bot, '/msg19 - chat 19\n')
 
         self.receive_message('/more')
         self.assertReplied(self.bot, 'No pending query...')
@@ -248,7 +228,7 @@ There are more groups, type /more to list 10 more results''')
         self.assertRaises(AssertionError, self.last_reply, self.bot)
 
         self.receive_message('/auth changeme')
-        self.assertReplied(self.bot, 'Welcome :-)')
+        self.assertReplied(self.bot, u'Welcome \U0001F60F')
 
         self.receive_message('/newpass otherpassword')
         self.assertReplied(
@@ -269,4 +249,20 @@ otherpassword''')
         self.assertRaises(AssertionError, self.last_reply, self.bot)
 
         self.receive_message('/auth otherpassword', sender=sender)
-        self.assertReplied(self.bot, 'Welcome :-)')
+        self.assertReplied(self.bot, u'Welcome \U0001F60F')
+
+    def test_message(self):
+        self.receive_message('/auth changeme')
+        self.assertReplied(self.bot, u'Welcome \U0001F60F')
+
+        self.receive_message('/msg1 hello')
+        self.assertReplied(self.bot, "'hello' sent to 1")
+
+        self.receive_message('/msg 1 bye')
+        self.assertReplied(self.bot, "'bye' sent to 1")
+
+        self.receive_message('/msg1')
+        self.assertReplied(self.bot, "And say what?")
+
+        self.receive_message('hey again')
+        self.assertReplied(self.bot, "'hey again' sent to 1")
